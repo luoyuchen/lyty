a634e987db8f91f15d7976d2f4cc13a2df56e474
luowei <lwalex@foxmail.com>
2017-05-16 17:47:28 +0800

    no message

--- pages/hot/hot.js
+++ pages/hot/hot.js
@@ -8,8 +8,19 @@ Page({
       postList:postsData.postList[0]
     })
       //console.log(postsData)  
-
-
+    
+    wx.showShareMenu({
+      withShareTicket: true,
+      success: function (res) {
+        // 分享成功
+        console.log('shareMenu share success')
+        console.log(res)
+      },
+      fail: function (res) {
+        // 分享失败
+        console.log(res)
+      }
+    })
     },
   onReady:function(){
     // 生命周期函数--监听页面初次渲染完成
@@ -36,11 +47,24 @@ Page({
     
   },
   onShareAppMessage: function() {
+    
     // 用户点击右上角分享
     return {
       title: '【专业服务！一手送签！】', // 分享标题
       desc: '', // 分享描述
-      path: 'pages/hot/hot' // 分享路径
+      path: 'pages/hot/hot', // 分享路径
+      success:function(res){
+        console.log(res)
+        console.log(res.shareTickets[0])
+        wx.getShareInfo({
+          shareTicket: res.shareTickets[0],
+          success:function(res){console.log(res)},
+          fail:function(res){console.log(res)}
+        })
+      },
+      fail:function(res){
+        console.log(res)
+      }
     }
   },
   callPhone:function(){
